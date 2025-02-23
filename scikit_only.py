7d66d820b65e8d69d89471e215b91e6643113359
Камиль Рахматуллин <mcmk@MacBook-Pro-Kamil.local>
2025-02-23 15:27:33 +0300

    upd

--- scikit_only.py
+++ scikit_only.py
@@ -1,12 +1,11 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_diabetes
-from sklearn.metrics import r2_score
 
 lr = LinearRegression()
 X, y = load_diabetes(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
 reg = lr.fit(X_train, y_train)
-print(r2_score(y_test, reg.predict(X_test)))
+print(reg.score(X_test, y_test))
 
 
